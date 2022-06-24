45e66b30e08588e4720293e45c96450f2fc1d89e
Lnx <yermin.linos@ug.edu.ec>
2022-06-24 17:01:03 -0500

    Header & Footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { PagePrincipalComponent } from './page-principal/page-principal.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PagePrincipalComponent
+    PagePrincipalComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
