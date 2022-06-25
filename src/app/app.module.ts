9559b19cb6e8620f80062d273f9f9a3ab96f9568
BrayanC22 <56985044+BrayanC22@users.noreply.github.com>
2022-06-25 16:12:05 -0500

    Mega Cambio prrs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,34 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {MatButtonModule} from '@angular/material/button';
+import {MatInputModule} from '@angular/material/input'
+import {MatCardModule} from '@angular/material/card'
+import {MatToolbarModule} from '@angular/material/toolbar'
+import {MatIconModule} from '@angular/material/icon';
+import {MatDialogModule} from '@angular/material/dialog';
+import {MatTableModule} from '@angular/material/table';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule } from './app-router.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { PagePrincipalComponent } from './page-principal/page-principal.component';
+import { OfertasComponent } from './ofertas/ofertas.component';
+import { AgregarOfertaComponent } from './agregar-oferta/agregar-oferta.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PagePrincipalComponent
+    PagePrincipalComponent,
+    OfertasComponent,
+    AgregarOfertaComponent,
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule
+    AppRoutingModule, MatButtonModule,
+    OfertasComponent,
+    BrowserAnimationsModule,AgregarOfertaComponent,MatInputModule, 
+    MatCardModule, MatToolbarModule, MatIconModule, MatDialogModule, MatTableModule
   ],
   providers: [],
   bootstrap: [AppComponent]
