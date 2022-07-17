f4f521ff16054ec19a1aec2b079eb0025ed11530
BrayanC22 <brayancalvopina22@gmail.com>
2022-07-17 14:54:48 -0500

    Ofertas Terminadas

--- src/app/Interfaces/OfertaInterface.ts
+++ src/app/Interfaces/OfertaInterface.ts
@@ -3,7 +3,7 @@ export interface OfertaInterface {
 
     //Imagen: string;
     Temporada: string;
-    Categorias: string;
+    Categorias: string; //Tipo de oferta.
     Precio: number;
     Descuento : number;
     Descripcion: string;
