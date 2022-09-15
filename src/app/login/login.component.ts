43ddf512279647317194bf07b87ff70d786cac20
YERMIN YAIR LINO SANCHEZ <yermin.linos@ug.edu.ec>
2022-09-15 17:58:10 -0500

    Oferta-Token

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -4,6 +4,8 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { MatDialogRef } from '@angular/material/dialog';
 import { NavigationExtras, Router } from '@angular/router';
 import { LoginService } from '../services/login.service';
+import { credenciales } from '../Interfaces/UsuarioLogin';
+import { NgLocaleLocalization } from '@angular/common';
 
 @Component({
   selector: 'app-login',
@@ -13,28 +15,26 @@ import { LoginService } from '../services/login.service';
 export class LoginComponent {
 
   alert: boolean = false;
+  usuarioTemp:any;
+  passwordTemp:any;
 
   usuarioLogin = new FormGroup({
-    usuario: new FormControl('', Validators.required),
+    nombreUsuario: new FormControl('', Validators.required),
     password: new FormControl('', Validators.required)
   })
 
   constructor(private router: Router, private dialogRef: MatDialogRef<LoginComponent>, private loginService: LoginService) { }
 
-
-
-
   onSubmit(){
-
-    if(this.loginService.login(this.usuarioLogin.value.usuario??'', this.usuarioLogin.value.password??'')){
-      this.router.navigate(['/libro']);
+    this.usuarioTemp = this.usuarioLogin.value.nombreUsuario;
+    
+    this.loginService.login(this.usuarioLogin.value as credenciales).subscribe((data:any)=>{
+      console.log(data);
+      localStorage.setItem('NombreUsuario',this.usuarioTemp);
+      localStorage.setItem('token_value',data);
+      this.router.navigate(['/Oferta']);
       this.dialogRef.close();
-    }
-    else {
-      this.alert = true;
-      setTimeout(() => this.alert = false, 4000);
+    },
+    (errorData)=> alert(errorData.console.error))
     }
   }
-
-
-}
